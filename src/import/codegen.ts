10265062d9c6466cbe72c79a027b8d36b18fa813
Christopher Rybicki <rybickic@amazon.com>
2021-08-18 10:23:05 -0700

    add return type, update snapshots

--- src/import/codegen.ts
+++ src/import/codegen.ts
@@ -172,7 +172,7 @@ export function generateConstruct(typegen: TypeGenerator, def: ApiObjectDefiniti
       code.line('/**');
       code.line(' * Renders the object to Kubernetes JSON.');
       code.line(' */');
-      code.openBlock('public toJson()');
+      code.openBlock('public toJson(): any');
       code.line('const resolved = super.toJson();');
       code.line();
       code.open('return {');
