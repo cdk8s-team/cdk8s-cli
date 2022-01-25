c880323df710d7d1b9f5543b3add8b43aae37a45
camba1 <peredo01@gmail.com>
2022-01-25 14:29:40 -0600

    Cleanup and formatting
    Signed-off-by: camba1 <peredo01@gmail.com>

--- test/util.test.ts
+++ test/util.test.ts
@@ -5,11 +5,6 @@ import { getFiles } from '../src/util';
 
 describe('getFiles', () => {
 
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
   test('test read files', async () => {
 
     //WHEN we have this folder structure
@@ -19,36 +14,36 @@ describe('getFiles', () => {
     // Create folders
     const baseDir: string = await promises.mkdtemp(path.join(tmpdir(), 'cdk8s-getFiles-test'));
     const emptySubDir: string = await promises.mkdtemp(path.join(baseDir, 'emptyDir'));
-    const PopulatedSubDir: string = await promises.mkdtemp(path.join(baseDir, 'populatedDir'));
-    const PopulatedSubDir2: string = await promises.mkdtemp(path.join(PopulatedSubDir, 'populatedDir2'));
+    const populatedSubDir: string = await promises.mkdtemp(path.join(baseDir, 'populatedDir'));
+    const populatedSubDir2: string = await promises.mkdtemp(path.join(populatedSubDir, 'populatedDir2'));
     // Do not create a folder in this path
     const nonDir: string = path.join(baseDir, 'nonDir');
     //Create Files
-    await promises.writeFile(path.join(PopulatedSubDir, `example.${extensionFilter}`), 'contents');
-    await promises.writeFile(path.join(PopulatedSubDir, `example2.${extensionFilter}`), 'contents');
-    await promises.writeFile(path.join(PopulatedSubDir2, `example3.${extensionFilter}`), 'contents');
-    await promises.writeFile(path.join(PopulatedSubDir2, 'example4.txt'), 'contents');
+    await promises.writeFile(path.join(populatedSubDir, `example.${extensionFilter}`), 'contents');
+    await promises.writeFile(path.join(populatedSubDir, `example2.${extensionFilter}`), 'contents');
+    await promises.writeFile(path.join(populatedSubDir2, `example3.${extensionFilter}`), 'contents');
+    await promises.writeFile(path.join(populatedSubDir2, 'example4.txt'), 'contents');
 
 
     // THEN
 
     // Get the contents of the different folders
-    const noFiles : string[] = await getFiles(emptySubDir);
-    const yamlFiles : string[] = await getFiles(PopulatedSubDir);
-    const NoFilesEither : string[] = await getFiles(nonDir);
+    const noFiles: string[] = await getFiles(emptySubDir);
+    const yamlFiles: string[] = await getFiles(populatedSubDir);
+    const noFilesEither: string[] = await getFiles(nonDir);
 
     // Empty sub directory should yield empty array
     expect(noFiles).toEqual([]);
 
     // Directory with files
     const expectedFiles: string [] = [
-      path.join(PopulatedSubDir, `example.${extensionFilter}`),
-      path.join(PopulatedSubDir, `example2.${extensionFilter}`),
-      path.join(PopulatedSubDir2, `example3.${extensionFilter}`),
+      path.join(populatedSubDir, `example.${extensionFilter}`),
+      path.join(populatedSubDir, `example2.${extensionFilter}`),
+      path.join(populatedSubDir2, `example3.${extensionFilter}`),
     ];
     expect(yamlFiles).toEqual(expectedFiles);
 
     //Bad directory name (non-existent) should yield an empty array
-    expect(NoFilesEither).toEqual([]);
+    expect(noFilesEither).toEqual([]);
   });
 });
