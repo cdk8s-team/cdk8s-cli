8d7b3c6413165c22fecd80393ed20451d02061bf
Eli Polonsky <epolon@amazon.com>
2022-09-30 10:13:34 +0300

    feat: manifest validation plugins (#479)
    This PR implements a new plugin mechanism for post synthesis manifest validation.
    
    Documentation:
    
    - https://github.com/cdk8s-team/cdk8s-validation-plugin-example/pull/1
    - https://github.com/cdk8s-team/cdk8s/pull/966
    
    Resolves https://github.com/cdk8s-team/cdk8s-cli/issues/460
    
    (cherry picked from commit bc55c69a77091a21967a5d76d3b103f7dfa20c5d)
    Signed-off-by: Eli Polonsky <epolon@amazon.com>

--- src/cli/cmds/synth.ts
+++ src/cli/cmds/synth.ts
@@ -1,8 +1,9 @@
-import * as path from 'path';
-import * as fs from 'fs-extra';
+import * as fs from 'fs';
+import * as yaml from 'yaml';
 import * as yargs from 'yargs';
-import { readConfigSync } from '../../config';
-import { synthApp, mkdtemp } from '../../util';
+import { readConfigSync, ValidationConfig } from '../../config';
+import { PluginManager } from '../../plugins/_manager';
+import { synthApp, mkdtemp, download, validateApp } from '../../util';
 
 const config = readConfigSync();
 
@@ -14,32 +15,63 @@ class Command implements yargs.CommandModule {
   public readonly builder = (args: yargs.Argv) => args
     .option('app', { default: config.app, required: true, desc: 'Command to use in order to execute cdk8s app', alias: 'a' })
     .option('output', { default: config.output, required: false, desc: 'Output directory', alias: 'o' })
-    .option('stdout', { type: 'boolean', required: false, desc: 'Write synthesized manifests to STDOUT instead of the output directory', alias: 'p' });
+    .option('stdout', { type: 'boolean', required: false, desc: 'Write synthesized manifests to STDOUT instead of the output directory', alias: 'p' })
+    .option('plugins-dir', { default: config.pluginsDirectory, required: false, desc: 'Directory to store cdk8s plugins.' })
+    .option('validate', { type: 'boolean', default: true, required: false, desc: 'Apply validation plugins on the resulting manifests (use --no-validate to disable)' })
+    .option('validation-reports-output-file', { required: false, desc: 'File to write a JSON representation of the validation reports to' });
+  ;
 
   public async handler(argv: any) {
+
     const command = argv.app;
     const outdir = argv.output;
     const stdout = argv.stdout;
+    const validate = argv.validate;
+    const pluginsDir = argv.pluginsDir;
+    const reportFile = argv.validationReportsOutputFile;
 
-    if (outdir !== config.output && stdout) {
+    if (outdir && outdir !== config.output && stdout) {
       throw new Error('\'--output\' and \'--stdout\' are mutually exclusive. Please only use one.');
     }
 
-    await fs.remove(outdir);
+    if (outdir) {
+      fs.rmSync(outdir, { recursive: true, force: true });
+    }
+
+    const validations = validate ? await fetchValidations() : undefined;
 
     if (stdout) {
       await mkdtemp(async tempDir => {
-        await synthApp(command, tempDir);
-
-        const manifests = (await fs.readdir(tempDir)).filter(f => path.extname(f) === '.yaml');
-        for (const f of manifests) {
-          fs.createReadStream(path.join(tempDir, f)).pipe(process.stdout);
+        const app = await synthApp(command, tempDir, stdout, validate);
+        for (const f of app.manifests) {
+          fs.createReadStream(f).pipe(process.stdout);
+        }
+        if (validations) {
+          const pluginManager = new PluginManager(pluginsDir);
+          await validateApp(app, stdout, validations, pluginManager, reportFile);
         }
       });
     } else {
-      await synthApp(command, outdir);
+      const manifests = await synthApp(command, outdir, stdout, validate);
+      if (validations) {
+        const pluginManager = new PluginManager(pluginsDir);
+        await validateApp(manifests, stdout, validations, pluginManager, reportFile);
+      }
     }
+
+
+  }
+
+}
+
+async function fetchValidations(): Promise<ValidationConfig[] | undefined> {
+  if (typeof(config.validations) === 'string') {
+    const content = await download(config.validations);
+    return yaml.parse(content) as ValidationConfig[];
+  } else {
+    return config.validations;
   }
 }
+
 
 module.exports = new Command();
