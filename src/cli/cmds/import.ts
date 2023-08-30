be8f44a766d0020f8b86d0920944572b3ea96770
epolon <epolon@amazon.com>
2023-08-30 19:56:47 +0300

    mid work
    Signed-off-by: epolon <epolon@amazon.com>

--- src/cli/cmds/import.ts
+++ src/cli/cmds/import.ts
@@ -22,7 +22,7 @@ class Command implements yargs.CommandModule {
     .example('cdk8s import mattermost:=mattermost_crd.yaml', 'Imports constructs for the mattermost cluster custom resource definition using a custom module name')
     .example('cdk8s import github:crossplane/crossplane@0.14.0', 'Imports constructs for a GitHub repo using doc.crds.dev')
 
-    .option('save', { type: 'boolean', required: false, default: true, desc: "Save the import URL in the 'imports' section of the cdk8s.yaml configuration file. Note that any comments made in the file will be lost.", alias: 's' })
+    .option('save', { type: 'boolean', required: false, default: true, desc: "Dont save the import URL in the 'imports' section of the cdk8s.yaml configuration file.", alias: 's' })
     .option('output', { default: DEFAULT_OUTDIR, type: 'string', desc: 'Output directory', alias: 'o' })
     .option('exclude', { type: 'array', desc: 'Do not import types that match these regular expressions. They will be represented as the "any" type (only for "k8s")' })
     .option('class-prefix', { type: 'string', desc: 'A prefix to add to all generated class names. By default, this is "Kube" for "k8s" imports and disabled for CRD imports. Use --no-class-prefix to disable. Must be PascalCase' })
