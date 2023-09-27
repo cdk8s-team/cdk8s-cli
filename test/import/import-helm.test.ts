bf2e5a5b0ee00910e1cf283ca7bc1b87fd793dfd
Vinayak Kukreja <vinakuk@amazon.com>
2023-09-26 19:27:05 -0700

    update PR
    Signed-off-by: Vinayak Kukreja <vinakuk@amazon.com>

--- test/import/import-helm.test.ts
+++ test/import/import-helm.test.ts
@@ -1,13 +1,18 @@
 import { testImportMatchSnapshot } from './util';
+import { Language } from '../../src/import/base';
 import { ImportHelm } from '../../src/import/helm';
 import { parseImports } from '../../src/util';
 
 const testChartUrl = 'helm:https://charts.bitnami.com/bitnami/mysql@9.10.10';
 
+
+// without schema
 describe('importing helm chart', () => {
   // TODO add multiple chart urls to test. Especially after fixing Json2Jsii issues
   const spec = parseImports(testChartUrl);
-  testImportMatchSnapshot(testChartUrl, async () => new ImportHelm(spec));
+
+  testImportMatchSnapshot('with typescript lanugage', async () => new ImportHelm(spec));
+  testImportMatchSnapshot('with python lanugage', async () => new ImportHelm(spec), { targetLanguage: Language.PYTHON });
 });
 
 describe('helm chart import validations', () => {
@@ -23,5 +28,12 @@ describe('helm chart import validations', () => {
     const spec = parseImports(testUrl);
 
     expect(() => {new ImportHelm(spec);}).toThrow('helm:https://charts.bitnami.com/bitnami/mysql@9.10.+FooBar\' for chart version: \'9.10.+FooBar\' does not follow SemVer-2(https://semver.org/).');
+  });
+
+  test('throws if url leads to no helm chart', () => {
+    const testUrl = 'helm:https://charts.bitnami.com/bitnami/mysql@1000.1000.1000';
+    const spec = parseImports(testUrl);
+
+    expect(() => {new ImportHelm(spec);}).toThrow();
   });
 });
