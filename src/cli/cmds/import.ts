92ee4ce6cc30693bd1349a4098fee37dd4535588
cdk8s-automation <81352262+cdk8s-automation@users.noreply.github.com>
2023-10-02 04:19:16 -0400

    feat: helm synthesis (#1195) (#1466)
    # Backport
    
    This will backport the following commits from `2.x` to `1.x`:
     - [feat: helm synthesis (#1195)](https://github.com/cdk8s-team/cdk8s-cli/pull/1195)
    
    
    
    ### Questions ?
    Please refer to the [Backport tool documentation](https://github.com/sqren/backport)

--- src/cli/cmds/import.ts
+++ src/cli/cmds/import.ts
@@ -1,7 +1,8 @@
 import * as yargs from 'yargs';
 import { readConfigSync, ImportSpec } from '../../config';
-import { PREFIX_DELIM, importDispatch } from '../../import/dispatch';
+import { importDispatch } from '../../import/dispatch';
 import { DEFAULT_API_VERSION } from '../../import/k8s';
+import { parseImports } from '../../util';
 
 const config = readConfigSync();
 
@@ -14,7 +15,7 @@ class Command implements yargs.CommandModule {
   public readonly aliases = ['gen', 'import', 'generate'];
 
   public readonly builder = (args: yargs.Argv) => args
-    .positional('SPEC', { default: config.imports, desc: 'import spec with the syntax [NAME:=]SPEC where NAME is an optional module name and supported SPEC are: k8s, crd.yaml, https://domain/crd.yaml, github:account/repo[@VERSION]).', array: true })
+    .positional('SPEC', { default: config?.imports, desc: 'import spec with the syntax [NAME:=]SPEC where NAME is an optional module name and supported SPEC are: k8s, crd.yaml, https://domain/crd.yaml, github:account/repo[@VERSION]).', array: true })
     .example('cdk8s import k8s', `Imports Kubernetes API objects to imports/k8s.ts. Defaults to ${DEFAULT_API_VERSION}`)
     .example('cdk8s import k8s --no-class-prefix', 'Imports Kubernetes API objects without the "Kube" prefix')
     .example('cdk8s import k8s@1.13.0', 'Imports a specific version of the Kubernetes API')
@@ -26,7 +27,7 @@ class Command implements yargs.CommandModule {
     .option('output', { default: DEFAULT_OUTDIR, type: 'string', desc: 'Output directory', alias: 'o' })
     .option('exclude', { type: 'array', desc: 'Do not import types that match these regular expressions. They will be represented as the "any" type (only for "k8s")' })
     .option('class-prefix', { type: 'string', desc: 'A prefix to add to all generated class names. By default, this is "Kube" for "k8s" imports and disabled for CRD imports. Use --no-class-prefix to disable. Must be PascalCase' })
-    .option('language', { default: config.language, demand: true, type: 'string', desc: 'Output programming language', alias: 'l', choices: LANGUAGES });
+    .option('language', { default: config?.language, demand: true, type: 'string', desc: 'Output programming language', alias: 'l', choices: LANGUAGES });
 
   public async handler(argv: any) {
     const classNamePrefix = argv.classPrefix === false ? '' : argv.classPrefix;
@@ -40,30 +41,6 @@ class Command implements yargs.CommandModule {
       save: argv.save,
     });
   }
-}
-
-function parseImports(spec: string): ImportSpec {
-  const splitImport = spec.split(PREFIX_DELIM);
-
-  // k8s@x.y.z
-  // crd.yaml
-  // url.com/crd.yaml
-  if (splitImport.length === 1) {
-    return {
-      source: spec,
-    };
-  }
-
-  // crd=crd.yaml
-  // crd=url.com/crd.yaml
-  if (splitImport.length === 2) {
-    return {
-      moduleNamePrefix: splitImport[0],
-      source: splitImport[1],
-    };
-  }
-
-  throw new Error('Unable to parse import specification. Syntax is [NAME:=]SPEC');
 }
 
 module.exports = new Command();
